1c9d12cd2d97f59d2315d50ad4c49ca9d968f10d
Thy Nguyen <tee@Thys-iMac.local>
2022-10-13 09:54:14 -0600

    new changes

---
+++ conditionals.js
@@ -0,0 +1,50 @@
+const raining = false;
+const cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+const temperature = 15;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+function whichSchool(age) {
+
+  if (age < 13) {
+    console.log("Elementary School");
+  } else if (age <= 18) {
+    console.log("Secondary School");
+  } else {
+    console.log("Lighthouse Labs")
+  }
+}
