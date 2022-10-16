6f7f43e3ef6111f402e60d95c4afc80ec485bde9
Thy Nguyen <tee@Thys-iMac.local>
2022-10-16 08:13:39 -0600

    Concatenate Arrays

---
+++ concat.js
@@ -0,0 +1,8 @@
+let concat = function joinFunc (arr1, arr2) {
+    return arr1.concat(arr2);
+  }
+  
+  console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+  console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+  console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+  console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
