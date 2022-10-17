55b6e428a6430a2e8bcb6e44163ea71dd6fe6094
Thy Nguyen <tee@Thys-iMac.local>
2022-10-16 20:38:29 -0600

    Trash to Treasure

---
+++ trash-to-treasure.js
@@ -0,0 +1,7 @@
+const smartGarbage = (trash, bins) => {
+    if (trash === 'waste') bins.waste++ 
+    else if (trash === 'recycling') bins.recycling++
+    else if (trash === 'compost') bins.compost++
+    return bins
+}
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 })); 
